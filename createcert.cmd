ce0c1f9a6b6f5671e8e75ef21bec5566f36d0891
Eric Vernie <ericv@microsoft.com>
2017-03-09 18:49:35 +0100

    Add cert

---
+++ createcert.cmd
@@ -0,0 +1,8 @@
+"C:\Program Files\Git\usr\bin\openssl.exe" genrsa -out localhost-key.pem 1024
+"C:\Program Files\Git\usr\bin\openssl.exe" req -new -key localhost-key.pem -out localhost.csr
+"C:\Program Files\Git\usr\bin\openssl.exe" x509 -req -in localhost.csr -signkey localhost-key.pem -out localhost-cert.pem
+
+
+•In order to run nodejs you need to create certificat using openssl openssl genrsa -out localhost-key.pem 1024 openssl req -new -key localhost-key.pem -out localhost.csr Country Name (2 letter code) [AU]:US State or Province Name (full name) [Some-State]:Washington Locality Name (eg, city) []:Redmond Organization Name (eg, company) [Internet Widgits Pty Ltd]:Contoso Organizational Unit Name (eg, section) []:corp Common Name (e.g. server FQDN or YOUR name) []:localhost Email Address []:admin@contoso.com
+
+Please enter the following 'extra' attributes to be sent with your certificate request A challenge password []: An optional company name []: openssl x509 -req -in localhost.csr -signkey localhost-key.pem -out localhost-cert.pem and install the certificat in the trust root certification authority
