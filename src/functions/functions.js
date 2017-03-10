3c9d9845b00c85e889f7f22429e68b9b154e9f13
Eric Vernie <ericv@microsoft.com>
2017-03-10 14:58:55 +0100

    adding command

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,3 +1,17 @@
 
 Office.initialize = function () {
 }
+
+
+function getSubject(event) {
+  var subject = Office.context.mailbox.item.subject;
+  
+  Office.context.mailbox.item.notificationMessages.addAsync("subject", {
+    type: "informationalMessage",
+    icon: "blue-icon-16",
+    message: "Subject: " + subject,
+    persistent: false
+  });
+  
+  event.completed();
+}
